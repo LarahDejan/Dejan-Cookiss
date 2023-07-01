01f1e8fde56b7be8377b88acdf608cfbedfe304b
Larah Dejan <130144801+LarahDejan@users.noreply.github.com>
2023-07-01 16:00:36 +0800

    Update Cookies.cs

--- Dejan-Cookiss/Cookies.cs
+++ Dejan-Cookiss/Cookies.cs
@@ -1,1 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookiss
+{
+    internal class Cookies
+
+    {
+        public string flavor;
+
+        public void ChocoCookies()
+        {
+            Console.WriteLine("Choco Cookies");
+        }
+
+        public void ChocoOatCookies()
+        {
+            Console.WriteLine("Choco Oat Cookies");
+        }
+
+        public void SupremeCookies()
+        {
+            Console.WriteLine("Supreme Cookies");
+
+        }
     }
+
+    internal class Ingredients
+    {
+        public int quantity;
+        public string ingredients;
+        public int measurement;
+
+        public void HowMany()
+        {
+            Console.WriteLine("How many cookie you want to make");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void ListIngre()
+        {
+            Console.WriteLine("Ingredients");
+
+        }
+    }
+}
