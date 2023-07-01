0f7ce2c2ae7b4f07bdd9397d30efdbad110fa7d1
Larah Dejan <130144801+LarahDejan@users.noreply.github.com>
2023-07-01 16:00:41 +0800

    Create Ingredients.cs

---
+++ Dejan-Cookiss/Ingredients.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cookiss
+{
+    internal class IngreDients
+    {
+        public int quantity { get;}
+        public string ingredients;
+        public int measurement;
+
+        public void HowMany()
+        {
+            Console.WriteLine("How many cookie you want to make");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        public void ListIngre()
+        {
+            Console.WriteLine("Ingredients");
+
+        }
+    }
+}
+
