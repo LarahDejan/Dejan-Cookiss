5b52b76d9d351203c0540e213cdbe0ad3503462e
Larah Dejan <130144801+LarahDejan@users.noreply.github.com>
2023-07-12 09:25:31 +0800

    changes

---
+++ Dejan-Cookiss/CookieRecipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cookiss
+{
+    public class CookieRecipe
+    {
+        public string Name { get; set; }
+        public List<string> Ingredients { get; set; }
+
+        public void DisplayIngredients()
+        {
+            Console.WriteLine("Ingredients:");
+            foreach (string ingredient in Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Dejan-Cookiss/Program.cs
+++ Dejan-Cookiss/Program.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-using System.ComponentModel.Design;
+using System.Collections.Generic;
 
 namespace Cookiss
 {
-    class Program
+    internal class Program
     {
-        static void Mymethod()
+        public static void Mymethod()
         {
             Console.WriteLine("ENJOY YOUR COOKISS!");
         }
-        static void Main()
 
+        public static void Main()
         {
             Console.WriteLine("Welcome to Cookiss!");
             Console.WriteLine("Are you ready to make cookies?");
             Console.WriteLine("Enter 1 to continue");
             string cookies = "1";
             string enterChoice = Console.ReadLine();
-
 
             while (enterChoice != "0")
             {
@@ -26,87 +25,83 @@ namespace Cookiss
                 if (result == "correct")
                 {
                     var menu = new List<string>()
-                        { "What Cookies Do You Want?","Enter 1 for Choco Cookies","Enter 2 for Choco Oat Cookies",
-                            "Enter 3 for Supreme Cookies","Enter 0 to exit"};
+                    {
+                        "What Cookies Do You Want?",
+                        "Enter 1 for Choco Cookies",
+                        "Enter 2 for Choco Oat Cookies",
+                        "Enter 3 for Supreme Cookies",
+                        "Enter 0 to exit"
+                    };
 
-                        foreach (string i in menu)
-                        {
+                    foreach (string i in menu)
+                    {
                         Console.WriteLine(i);
-                        }
-               
+                    }
+
                     enterChoice = Console.ReadLine();
 
                     switch (enterChoice)
                     {
                         case "1":
                             Console.WriteLine("Choco Cookies");
-                            Console.WriteLine("  ");
-                            Console.WriteLine("How many cookies you want to make?");
-                            int numCookies = Convert.ToInt32(Console.ReadLine());
-                            int butter = 10*numCookies;
-                            int sugar = 14*numCookies;
-                            int egg = 6*numCookies;
-                            int vanilla = 1 * numCookies;
-                            int flour = 14 * numCookies;
-                            int oat = 10 * numCookies;
-                            double salt = 0.25 * numCookies;
-                            double powder = 0.12 * numCookies;
-                            double soda = 0.12 * numCookies;
-                            int chocolate = 15 * numCookies;
-                            int nuts = 8 * numCookies;
-                            int raisins = 8 * numCookies;
+                            Console.WriteLine();
 
-                            Console.WriteLine("Ingredients");
-                            Console.WriteLine("Butter = " + butter + " grams");
-                            Console.WriteLine("Dark or Light Brown Sugar = " + sugar + " grams");
-                            Console.WriteLine("Large egg = " + egg + " grams");
-                            Console.WriteLine("Vanilla Extract = " + vanilla + " grams");
-                            Console.WriteLine("All-Purpose Flour = " + flour + " grams");
-                            Console.WriteLine("Salt = " + salt + " grams");
-                            Console.WriteLine("Baking Powder = " + powder + "grams");
-                            Console.WriteLine("Baking Soda = " + soda + " grams");
-                            Console.WriteLine("Chocolate Chip or Bar = " + chocolate + "grams");
-                            Console.WriteLine("    ");
+                            var choco = new List<string>()
+                            {
+                                "Ingredients:",
+                                "Butter",
+                                "Dark or Light Brown Sugar",
+                                "Large egg",
+                                "Vanilla Extract",
+                                "All-Purpose Flour",
+                                "Salt",
+                                "Baking Powder",
+                                "Baking Soda",
+                                "Chocolate Chip or Bar"
+                            };
+
+                            foreach (string i in choco)
+                            {
+                                Console.WriteLine(i);
+                            }
+
+                            Console.WriteLine();
                             Mymethod();
-                            Console.WriteLine("    ");
+                            Console.WriteLine();
 
                             Console.WriteLine("Enter 1 to Return");
                             Console.WriteLine("Enter 0 to Exit");
-                            
+
                             enterChoice = Console.ReadLine();
                             break;
 
                         case "2":
                             Console.WriteLine("Choco Oat Cookies");
-                            Console.WriteLine("  ");
-                            Console.WriteLine("How many cookies you want to make?");
-                            numCookies = Convert.ToInt32(Console.ReadLine());
-                            butter = 10*numCookies;
-                            sugar = 14*numCookies;
-                            egg = 6*numCookies;
-                            vanilla = 1 * numCookies;
-                            flour = 14 * numCookies;
-                            oat = 10 * numCookies;
-                            salt = 0.25 * numCookies;
-                            powder = 0.12 * numCookies;
-                            soda = 0.12 * numCookies;
-                            chocolate = 15 * numCookies;
-                            nuts = 8 * numCookies;
-                            raisins = 8 * numCookies;
-                            Console.WriteLine("Ingredients");
-                            Console.WriteLine("Butter = " + butter + " grams");
-                            Console.WriteLine("Dark or Light Brown Sugar = " + sugar + " grams");
-                            Console.WriteLine("Large egg = " + egg + " grams");
-                            Console.WriteLine("Vanilla Extract = " + vanilla + " grams");
-                            Console.WriteLine("All-Purpose Flour = " + flour + " grams");
-                            Console.WriteLine("Whole Grain Oats = " + oat + "grams");
-                            Console.WriteLine("Salt = " + salt + " grams");
-                            Console.WriteLine("Baking Powder = " + powder + "grams");
-                            Console.WriteLine("Baking Soda = " + soda + " grams");
-                            Console.WriteLine("Chocolate Chip or Bar = " + chocolate + "grams");
-                            Console.WriteLine("    ");
+                            Console.WriteLine();
+
+                            var oat = new List<string>()
+                            {
+                                "Ingredients:",
+                                "Butter",
+                                "Dark or Light Brown Sugar",
+                                "Large egg",
+                                "Vanilla Extract",
+                                "All-Purpose Flour",
+                                "Salt",
+                                "Baking Powder",
+                                "Baking Soda",
+                                "Chocolate Chip or Bar",
+                                "Whole Grain Oats"
+                            };
+
+                            foreach (string i in oat)
+                            {
+                                Console.WriteLine(i);
+                            }
+
+                            Console.WriteLine();
                             Mymethod();
-                            Console.WriteLine("    ");
+                            Console.WriteLine();
                             Console.WriteLine("Enter 1 to return");
                             Console.WriteLine("Enter 0 to Exit");
 
@@ -115,42 +110,39 @@ namespace Cookiss
 
                         case "3":
                             Console.WriteLine("Supreme Cookies");
-                            Console.WriteLine("  ");
-                            Console.WriteLine("How many cookies you want to make?");
-                            numCookies = Convert.ToInt32(Console.ReadLine());
-                            butter = 10*numCookies;
-                            sugar = 14*numCookies;
-                            egg = 6*numCookies;
-                            vanilla = 1 * numCookies;
-                            flour = 14 * numCookies;
-                            oat = 10 * numCookies;
-                            salt = 0.25 * numCookies;
-                            powder = 0.12 * numCookies;
-                            soda = 0.12 * numCookies;
-                            chocolate = 15 * numCookies;
-                            nuts = 8 * numCookies;
-                            raisins = 8 * numCookies;
-                            Console.WriteLine("Ingredients");
-                            Console.WriteLine("Butter = " + butter + " grams");
-                            Console.WriteLine("Dark or Light Brown Sugar = " + sugar + " grams");
-                            Console.WriteLine("Large egg = " + egg + " grams");
-                            Console.WriteLine("Vanilla Extract = " + vanilla + " grams");
-                            Console.WriteLine("All-Purpose Flour = " + flour + " grams");
-                            Console.WriteLine("Whole Grain Oats = " + oat + "grams");
-                            Console.WriteLine("Salt = " + salt + " grams");
-                            Console.WriteLine("Baking Powder = " + powder + "grams");
-                            Console.WriteLine("Baking Soda = " + soda + " grams");
-                            Console.WriteLine("Chocolate Chip or Bar = " + chocolate + " grams");
-                            Console.WriteLine("Nuts = " + nuts + " grams");
-                            Console.WriteLine("Raisins = " + raisins + " grams");
-                            Console.WriteLine("    ");
+                            Console.WriteLine();
+
+                            var super = new List<string>()
+                            {
+                                "Ingredients:",
+                                "Butter",
+                                "Dark or Light Brown Sugar",
+                                "Large egg",
+                                "Vanilla Extract",
+                                "All-Purpose Flour",
+                                "Salt",
+                                "Baking Powder",
+                                "Baking Soda",
+                                "Chocolate Chip or Bar",
+                                "Whole Grain Oats",
+                                "Nuts",
+                                "Raisins"
+                            };
+
+                            foreach (string i in super)
+                            {
+                                Console.WriteLine(i);
+                            }
+
+                            Console.WriteLine();
                             Mymethod();
-                            Console.WriteLine("    ");
+                            Console.WriteLine();
                             Console.WriteLine("Enter 1 to return");
                             Console.WriteLine("Enter 0 to Exit");
 
                             enterChoice = Console.ReadLine();
                             break;
+
                         default:
                             break;
                     }
@@ -160,14 +152,11 @@ namespace Cookiss
                     Console.WriteLine("Incorrect choice");
                     Console.WriteLine("Please try again or press 0 to exit");
 
-                    Console.Write("Enter 1 to continue");
+                    Console.Write("Enter 1 to continue: ");
                     enterChoice = Console.ReadLine();
                 }
-
             }
         }
     }
 }
-    
-
 
